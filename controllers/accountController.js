bb5802efa8af79fd8d63cd22588f18a0afda3c8a
karlarum <131808949+karlarum@users.noreply.github.com>
2024-12-03 23:13:31 -0600

    process registration *missed this page earlier*

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,5 @@
-const utilities = require("../utilities")
+const accountModel = require("../models/account-model")
+const utilities = require("../utilities/")
 
 /* ****************************************
 *  Deliver login view
@@ -11,6 +12,38 @@ async function buildLogin(req, res, next) {
       message: "",
     })
   }
+
+  /* ****************************************
+*  Process Registration
+* *************************************** */
+async function registerAccount(req, res) {
+  let nav = await utilities.getNav()
+  const { account_firstname, account_lastname, account_email, account_password } = req.body
+
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  )
+
+  if (regResult) {
+    req.flash(
+      "notice",
+      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+    )
+    res.status(201).render("account/login", {
+      title: "Login",
+      nav,
+    })
+  } else {
+    req.flash("notice", "Sorry, the registration failed.")
+    res.status(501).render("account/register", {
+      title: "Registration",
+      nav,
+    })
+  }
+}
 
 /* ****************************************
 *  Deliver registration view
@@ -25,4 +58,4 @@ async function buildRegister(req, res, next) {
   })
 }
 
-module.exports = { buildLogin, buildRegister }
+module.exports = { buildLogin, registerAccount, buildRegister }
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const errorRoute = require("./routes/errorRoute")
 const session = require("express-session")
 const pool = require('./database/')
 const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -37,7 +38,8 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 /* ***********************
  * View Engine and Templates
  *************************/
